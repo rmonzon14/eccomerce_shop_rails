9424eff99ca4df10d2217a6ae9b38539d323b6ea
Red <redkmonzon30@gmail.com>
2023-11-30 20:56:15 -0600

    Add hom controller with index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Set default route to the home page
+  root 'home#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources 'artists', only: %i[index show]
 end
