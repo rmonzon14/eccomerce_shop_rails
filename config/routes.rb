d99c453b7bccfbd97c5c9e2172f6d867cf1c6892
Red <redkmonzon30@gmail.com>
2023-12-01 02:00:23 -0600

    Display products data to the index and show controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
   # Set default route to the home page
   root 'home#index'
 
+  # products page route
+  resources 'products', only: %i[index show]
 end
