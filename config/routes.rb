04cb75aa521ea14ac2c5c4bf218c6bbbb1dd16b5
Red <redkmonzon30@gmail.com>
2023-12-03 16:39:15 -0600

    Remove unnecesarry route code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'brands/index'
-  get 'brands/show'
+
   # Set default route to the home page
   root 'home#index'
 
