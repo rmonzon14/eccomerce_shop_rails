a157f11988ce2ba97c3b1c2db07d5b528c9de4ed
Red <redkmonzon30@gmail.com>
2023-12-04 17:50:31 -0600

    Add image attachment for horse model

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,18 +1,14 @@
 ActiveAdmin.register Product do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
   permit_params :product_name, :resell_price, :retail_price, :description, :released_date, :is_used, :color, :image_path, :brand_id, :product_gender_id, :category_id, :image
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:product_name, :resell_price, :retail_price, :description, :released_date, :is_used, :color, :image_path, :brand_id, :product_gender_id, :category_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 
 end
