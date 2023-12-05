62f98c4c457245b9f112259cd5cee8db7fa88c2f
Red <redkmonzon30@gmail.com>
2023-12-05 11:03:49 -0600

    Add search controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
@@ -13,4 +14,6 @@ Rails.application.routes.draw do
 
   # brands page route
   resources 'brands', only: %i[index show]
+
+  resources "search", only: :index
 end
