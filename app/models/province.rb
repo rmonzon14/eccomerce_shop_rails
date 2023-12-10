f2a5fcd9ed23ac9ff37ccbaaf0739fd673ea3d8f
Red <redkmonzon30@gmail.com>
2023-12-10 00:52:21 -0600

    Add validations and associations on users, user_infos, and province

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,5 @@
 class Province < ApplicationRecord
+  has_many :user_infos
+
+  validates :province_name, presence: true
 end
--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -1,4 +1,6 @@
 class UserInfo < ApplicationRecord
   belongs_to :user
   belongs_to :province
+
+  validates :first_name, :last_name, :phone_number, :postal_code, :city, presence: true
 end
